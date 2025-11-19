4fd351756ae09604631a7ca71a4a9dd0b46c4194
iza <59828082+izadoesdev@users.noreply.github.com>
2025-11-19 17:17:59 +0200

    feat: init rust

---
+++ rust/ingestion/target/debug/deps/ingestion-14b08ebe416e96d9.d
@@ -0,0 +1,6 @@
+/Users/iza/Dev/Databuddy/rust/ingestion/target/debug/deps/ingestion-14b08ebe416e96d9.d: src/main.rs src/models.rs
+
+/Users/iza/Dev/Databuddy/rust/ingestion/target/debug/deps/ingestion-14b08ebe416e96d9: src/main.rs src/models.rs
+
+src/main.rs:
+src/models.rs:
---
+++ rust/ingestion/target/debug/deps/ingestion-c7cbfa06dbe894b2.d
@@ -0,0 +1,5 @@
+/Users/iza/Dev/Databuddy/rust/ingestion/target/debug/deps/ingestion-c7cbfa06dbe894b2.d: src/main.rs
+
+/Users/iza/Dev/Databuddy/rust/ingestion/target/debug/deps/ingestion-c7cbfa06dbe894b2: src/main.rs
+
+src/main.rs:
